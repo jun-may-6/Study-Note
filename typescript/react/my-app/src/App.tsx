864ad2a6e2c81b50a6449c400ca45febff8f078e
ahmin030 <ahmin030@naver.com>
2024-07-24 00:38:54 +0900

    typescript react

--- typescript/react/my-app/src/App.tsx
+++ typescript/react/my-app/src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Store } from './Store';
+import { Address, Restaurant } from './model/restaurant';
+import { BestMenu } from './BestMenu';
 
-let data = {
+let data:Restaurant = {
   name: '식당',
-  cateagory: '한식',
+  category: '한식',
   address: {
     city: '인천',
     detail: '어딘가',
@@ -16,9 +18,20 @@ let data = {
 }
 
 const App:React.FC = () => {
+  /* 제네릭 문법을 사용하여 state 의 형태도 정해줄 수 있다. */
+  const [myRestaurant, setMyRestaurant] = useState<Restaurant>(data);
+  const changeAddress = (address:Address) => {
+    setMyRestaurant(state=>({...state, address: address}))
+  }
+
+  const showBestMenuName = (name:string):string=>{
+    return name;
+  }
+
   return (
     <div className='App'>
-      <Store info={data}/>
+      <Store info={data} changeAddress={changeAddress}/>
+      <BestMenu name="불고기 피자" category="피자" showBestMenuName={showBestMenuName}/>
     </div>
   )
 }
--- typescript/react/my-app/src/Store.tsx
+++ typescript/react/my-app/src/Store.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Address, Restaurant } from './model/restaurant';
 
-export const Store:React.FC = () => {
+/* 여기서만 쓰이는 props 타입 */
+/* 인터페이스를 통한 타입 구현 */
+interface OwnProps{
+    info:Restaurant,
+    /* 함수의 타입을 정해줄 수 있다. */
+    changeAddress(address:Address):void
+}
+
+export const Store:React.FC<OwnProps> = ({info, changeAddress}) => {
     return (
-        <div>Store</div>
+        <div>{info.name}</div>
     )
 }
