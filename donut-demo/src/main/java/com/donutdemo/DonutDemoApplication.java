e3d0dbfb08e42415f6e8f1dd4008cb017e4fded8
jun <ahmin030@naver.com>
2024-09-22 23:31:25 +0900

    demo

---
+++ donut-demo/src/main/java/com/donutdemo/DonutDemoApplication.java
@@ -0,0 +1,13 @@
+package com.donutdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonutDemoApplication.class, args);
+    }
+
+}
